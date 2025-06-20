73e451adcc878b019e885fc8ac1b9107f57f9ee2
ZhanxKer <155804522+ZhanxKer@users.noreply.github.com>
2025-06-20 23:53:14 +0800

    Update panel.php
    更换为于大陆速度更优的URL

--- pages/panel.php
+++ pages/panel.php
@@ -19,7 +19,7 @@ $rs = Database::querySingleLine("users", Array("username" => $_SESSION['user']))
 		<!-- Font Awesome Icons -->
 		<link rel="stylesheet" href="assets/panel/plugins/fontawesome-free/css/all.min.css">
 		<!-- IonIcons -->
-		<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+		<link rel="stylesheet" href="https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/ionicons/2.0.1/css/ionicons.min.css">
 		<!-- Theme style -->
 		<link rel="stylesheet" href="assets/panel/dist/css/adminlte.min.css">
 		<!-- Google Font: Source Sans Pro -->
